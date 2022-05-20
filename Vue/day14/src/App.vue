0e35a3bc2f708ffba3da5adfc84162dd5477b15f
Taemin <jtm990506@gmail.com>
2022-05-20 23:14:56 +0900

    add Vue day14

---
+++ Vue/day14/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <MyLoginPage />
+  </div>
+</template>
+
+<script>
+import MyLoginPage from "./components/MyLoginPage.vue";
+export default {
+  name: "App",
+  components: {
+    MyLoginPage,
+  },
+};
+</script>
